b125b752d1391a62b90a6ae98fac3cca2bfafedd
Robert Anderson <robaandersonjr@gmail.com>
2026-07-13 12:16:07 -0700

    Add featured projects section with Anime Search App, Ultraverse, and Habit Stacking

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ function moveBackground(event) {
   for (let i = 0; i < shapes.length; ++i) {
     const isOdd = i % 2 !== 0;
     const boolInt = isOdd ? -1 : 1;
+    // Added rotate after tutorial
     shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px) rotate(${x * boolInt * 10}deg)`
   }
 }
@@ -30,12 +31,12 @@ function contact(event) {
   const success = document.querySelector(".modal__overlay--success");
   loading.classList += " modal__overlay--visible";
   emailjs
-    .sendForm(
-      "service_80ih0if",
-      "template_d9refyl",
-      event.target,
-      "user_K1PoFs8pB2YVWStDxrUls"
-    )
+  .sendForm(
+    "service_rtetb7s",
+    "template_4k8cjpm",
+    event.target,
+    "n2buSVqsc0VzX-mpL"
+  )
     .then(() => {
       loading.classList.remove("modal__overlay--visible");
       success.classList += " modal__overlay--visible";
@@ -43,7 +44,7 @@ function contact(event) {
     .catch(() => {
       loading.classList.remove("modal__overlay--visible");
       alert(
-        "The email service is temporarily unavailable. Please contact me directly on email@email.com"
+        "The email service is temporarily unavailable. Please contact me directly on anderson.robert.andrew@outlook.com"
       );
     });
 }
